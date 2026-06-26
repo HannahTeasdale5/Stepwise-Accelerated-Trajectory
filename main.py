a17074849aa23b558f76527d425438fe284dcbb6
‘HannahTeasdale5’ <hannah@teasdaleclan.co.uk>
2026-06-26 17:12:37 +0100

    Works for arrays of t, all coordinate time components seem to work.

--- main.py
+++ main.py
@@ -1,45 +1,78 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def main():
-    print("Main")
-    a = 0.5 #must be below c
-    T = 10
-    t = np.linspace(0, 2*T, 1000)
+    x_max = 0.005
+    v_max = 0.4 #aT/c needs to be between 0 and 1 so v_max <= c/2, not sure why the factor of 1/2
+    a0 = (v_max**2)/x_max
+    T = 2*x_max/v_max
+    t = np.linspace(0, 2 * T, 10000)
     c = 1
-    tao = proper_time(a, t, T, c)
-    print(tao)
+    tao = proper_time(a0, t, T, c)
+    x_t = trajectory_t(a0, t, T)
+    v_t = velocity_t(a0, t, T)
+    a_t = acceleration_t(a0, t, T)
 
-def acceleration (a, t, T):
-    if t < T/2:
-        return a
-    elif t < 3*T/2:
-        return -a
-    else:
-        return a
+    plt.plot(t, tao)
+    plt.title("Tao")
+    plt.show()
+    plt.plot(t, x_t)
+    plt.title("X")
+    plt.show()
+    plt.plot(t, v_t)
+    plt.title("V")
+    plt.show()
+    plt.plot(t, a_t)
+    plt.title("A")
+    plt.show()
 
-def velocity (a, t, T):
-    if t < T / 2:
-        return a*t
-    elif t < 3 * T / 2:
-        return a*(T-t)
-    else:
-        return a*(t-2*T)
+
+
+def acceleration_t(a0, t, T):
+    low_t = a0 * (t <= T / 2).astype(int)
+    medium_t = -a0 * ((t > T / 2) & (t <= 3 * T / 2)).astype(int)
+    high_t = a0 * (t > 3 * T / 2).astype(int)
+    return low_t + medium_t + high_t
+
+
+def velocity_t(a0, t, T):
+    low_t = a0 * t * (t <= T / 2).astype(int)
+    medium_t = a0 * (T - t) * ((t > T / 2) & (t <= 3 * T / 2)).astype(int)
+    high_t = a0 * (t - 2*T) * (t > 3 * T / 2).astype(int)
+    return low_t + medium_t + high_t
+
+def trajectory_t(a0, t, T):
+    low_t = (0.5)*a0 *(t**2) * (t <= T / 2).astype(int)
+    medium_t = (a0*T*t - 0.5*a0*(t**2) - (0.25)*a0*(T**2)) * ((t > T / 2) & (t <= 3 * T / 2)).astype(int)
+    high_t = (0.5*a0*((t-2*T)**2)) * (t > 3 * T / 2).astype(int)
+    return low_t + medium_t + high_t
+
+def trajectory_tao(a, tao, T, c):
+    #solve for coordinate time t at proper time tao
+    t = 0
+    #calculate the accelration at coordinate time t
+    a = acceleration_t(a, t, T)
+    #find the four vector due to this acceleration
+    x = (1/a) * np.cosh(a*tao)
+    t = (1/a) * np.sinh(a*tao)
+    return [c*t, x]
+
 
 def antiderivative(k, u):
-    root = (1 - k*u)**(-1/2)
-    return (u/2)*root + (1/(2*k))*np.arcsin(k*u)
+    root = (1 - k * u) ** (-1/2)
+    return (u / 2) * root + (1 / (2 * k)) * np.arcsin(k * u)
 
-def g_function(a, c, t):
-    return antiderivative(a/c, t)
 
-def proper_time(a, t, T, c):
-    if t < T / 2:
-        return g_function(a, c, t)
-    elif t < 3 * T / 2:
-        return 2*g_function(a, c, T/2) + g_function(a, c, t-T)
-    else:
-        return 4*g_function(a, c, T/2) + g_function(a, c, t-2*T)
+def g_function(a0, c, t):
+    return antiderivative(a0 / c, t)
 
+
+def proper_time(a0, t, T, c):
+    low_t = (t <= T / 2).astype(int)
+    medium_t = ((t > T / 2) & (t <= 3 * T / 2)).astype(int)
+    high_t = (t > 3 * T / 2).astype(int)
+    return low_t*(g_function(a0, c, t)) + medium_t*(2 * g_function(a0, c, T / 2) + g_function(a0, c, t - T)) + (4 * g_function(a0, c, T / 2) + g_function(a0, c, t - 2 * T))*high_t
 
 if __name__ == "__main__":
     main()
